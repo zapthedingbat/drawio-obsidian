572b0d1871ac56949da18c892b866f93e217a623
Sam Greenhalgh <sam@zapthedingbat.com>
2021-09-02 16:05:43 +0100

    clean up obsidian typings

--- typings/obsidian.d.ts
+++ typings/obsidian.d.ts
@@ -4,7 +4,7 @@ declare module "obsidian" {
   export interface FileExplorerView extends View {
     headerDom: {
       addNavButton: (
-        icon: String,
+        icon: string,
         title: string,
         onClick: () => void
       ) => HTMLElement;
@@ -24,36 +24,25 @@ declare module "obsidian" {
       ctx?: any
     ): EventRef;
   }
-
   export interface ViewRegistry {
     trigger(eventName: string, ...args: any[]): void;
     typeByExtension: { [key: string]: string };
   }
-
-  export interface CustomCss {
-    styleEl: HTMLStyleElement;
-  }
-
   export interface MenuItem {
     dom: HTMLElement;
   }
-
   export interface Menu {
     dom: HTMLElement;
     items: MenuItem[];
   }
-
   export interface LoadProgress {
     setMessage(message: string): this;
     show(): this;
     hide(): this;
   }
-
   export interface App {
-    customCss: CustomCss;
     internalPlugins: InternalPlugins;
     viewRegistry: ViewRegistry;
-    account: any;
     loadProgress: LoadProgress;
   }
 }
